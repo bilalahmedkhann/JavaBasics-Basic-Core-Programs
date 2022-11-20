60b9a0f094ab2b0237aa6fb6e8e5f8a1330bb0ae
Bilal Ahmed Khan <bilal.ahmed98@gmail.com>
2022-11-20 12:14:08 +0530

    ADDED: Code to flip Coin and print percentage of Heads and Tails

--- BasicCorePrograms.java
+++ BasicCorePrograms.java
@@ -1,5 +1,33 @@
+import java.util.*;
+
 public class BasicCorePrograms {
     public static void main(String[] args) {
-        System.out.println("Welcome to Basic Core Programs");
+        //declaration
+        int head = 0;
+        int tail = 0;
+
+        // Get input from the user
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number of time to flip the coin");
+        int num = sc.nextInt();
+
+        // check  the condition
+        for (int i = 1; i <= num; i++) {
+            double a = Math.random();
+            if (a < 0.5) {
+                tail++;
+            } else {
+                head++;
+            }
+        }
+        System.out.println("Head = " + head);
+        System.out.println("Tail = " + tail);
+
+        // Calculating Percentage for Head and Tail
+        int headPercent = (head * 100) / num;
+        int tailPercent = (tail * 100) / num;
+
+        System.out.println("HeadPercentage = " + headPercent + "%");
+        System.out.println("TailPercentage = " + tailPercent + "%");
     }
 }
