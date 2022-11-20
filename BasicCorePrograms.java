5c3c4ce58c5cdb5aad876f0476200829fae61aa6
Bilal Ahmed Khan <bilal.ahmed98@gmail.com>
2022-11-20 20:00:40 +0530

    ADDED: Code to print the Nth Harmonic Value

--- BasicCorePrograms.java
+++ BasicCorePrograms.java
@@ -7,7 +7,6 @@ public class BasicCorePrograms {
         System.out.println("Enter the Power of 2");
         int N = sc.nextInt();
         int num = 1;
-
         //Condition to get power of 2
         for (int i = 1; i <= N; i++) {
             if (N < 31) {
