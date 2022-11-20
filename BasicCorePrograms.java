68802145303be11713b944735846e48ed7985147
Bilal Ahmed Khan <bilal.ahmed98@gmail.com>
2022-11-20 12:40:51 +0530

    ADDED: Code to check leap year or not

--- BasicCorePrograms.java
+++ BasicCorePrograms.java
@@ -2,32 +2,16 @@ import java.util.*;
 
 public class BasicCorePrograms {
     public static void main(String[] args) {
-        //declaration
-        int head = 0;
-        int tail = 0;
-
-        // Get input from the user
+        //Taking input year from user
+        System.out.println("Input year to check leap year or not");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Number of time to flip the coin");
-        int num = sc.nextInt();
+        int year = sc.nextInt();
 
-        // check  the condition
-        for (int i = 1; i <= num; i++) {
-            double a = Math.random();
-            if (a < 0.5) {
-                tail++;
-            } else {
-                head++;
-            }
+        //Checking conditions for leap year
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year +" is leap year");
+        } else {
+            System.out.println(year +" is not a leap year");
         }
-        System.out.println("Head = " + head);
-        System.out.println("Tail = " + tail);
-
-        // Calculating Percentage for Head and Tail
-        int headPercent = (head * 100) / num;
-        int tailPercent = (tail * 100) / num;
-
-        System.out.println("HeadPercentage = " + headPercent + "%");
-        System.out.println("TailPercentage = " + tailPercent + "%");
     }
 }
